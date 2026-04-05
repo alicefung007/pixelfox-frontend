0b5445c12089678c2035507d0cc86ffc973c5bf4
alicefung007 <alicefung2026@gmail.com>
2026-04-06 00:21:37 +0800

    feat: add multiple system palettes
    - Add 12 new palettes (MARD variants and community palettes)
    - Export all palettes in index file
    - Add i18n strings for all new palette names

--- src/lib/palettes/index.ts
+++ src/lib/palettes/index.ts
@@ -1,12 +1,51 @@
 import type { PaletteDefinition } from "@/lib/palettes/types";
 import { MARD_PALETTE } from "@/lib/palettes/mard";
 import { MARD24_PALETTE } from "@/lib/palettes/mard24";
+import { MARD221_PALETTE } from "@/lib/palettes/mard221";
+import { MARD48_PALETTE } from "@/lib/palettes/mard48";
+import { MARD72_PALETTE } from "@/lib/palettes/mard72";
+import { MARD96_PALETTE } from "@/lib/palettes/mard96";
+import { MARD120_PALETTE } from "@/lib/palettes/mard120";
+import { MARD144_PALETTE } from "@/lib/palettes/mard144";
+import { MARD216_PALETTE } from "@/lib/palettes/mard216";
+import { MARD264_PALETTE } from "@/lib/palettes/mard264";
+import { MIXIAOWO_PALETTE } from "@/lib/palettes/mixiaowo";
+import { COCO_PALETTE } from "@/lib/palettes/coco";
+import { MANMAN_PALETTE } from "@/lib/palettes/manman";
+import { PANPAN_PALETTE } from "@/lib/palettes/panpan";
 
 export type { PaletteDefinition, PaletteSwatch } from "@/lib/palettes/types";
 export { MARD_PALETTE } from "@/lib/palettes/mard";
 export { MARD24_PALETTE } from "@/lib/palettes/mard24";
+export { MARD221_PALETTE } from "@/lib/palettes/mard221";
+export { MARD48_PALETTE } from "@/lib/palettes/mard48";
+export { MARD72_PALETTE } from "@/lib/palettes/mard72";
+export { MARD96_PALETTE } from "@/lib/palettes/mard96";
+export { MARD120_PALETTE } from "@/lib/palettes/mard120";
+export { MARD144_PALETTE } from "@/lib/palettes/mard144";
+export { MARD216_PALETTE } from "@/lib/palettes/mard216";
+export { MARD264_PALETTE } from "@/lib/palettes/mard264";
+export { MIXIAOWO_PALETTE } from "@/lib/palettes/mixiaowo";
+export { COCO_PALETTE } from "@/lib/palettes/coco";
+export { MANMAN_PALETTE } from "@/lib/palettes/manman";
+export { PANPAN_PALETTE } from "@/lib/palettes/panpan";
 
-export const SYSTEM_PALETTES = [MARD_PALETTE, MARD24_PALETTE] as const satisfies readonly PaletteDefinition[];
+export const SYSTEM_PALETTES = [
+  MARD_PALETTE,
+  MARD264_PALETTE,
+  MARD216_PALETTE,
+  MARD144_PALETTE,
+  MARD120_PALETTE,
+  MARD96_PALETTE,
+  MARD72_PALETTE,
+  MARD48_PALETTE,
+  MARD24_PALETTE,
+  MARD221_PALETTE,
+  MIXIAOWO_PALETTE,
+  COCO_PALETTE,
+  MANMAN_PALETTE,
+  PANPAN_PALETTE
+] as const satisfies readonly PaletteDefinition[];
 
 export const SYSTEM_PALETTE_BY_ID = Object.fromEntries(
   SYSTEM_PALETTES.map((p) => [p.id, p])
